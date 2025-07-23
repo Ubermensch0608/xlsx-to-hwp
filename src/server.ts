14da92f2d2f78b608fc77d9bbd08037e03a77e18
우종원 <danielwoo@mathflat.com>
2025-07-24 06:30:42 +0900

    feat: 파일 업로드 기능 구현

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,34 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import path from "path";
+import multer from "multer";
 
 const app = express();
 
+// middleware
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// multer
+const upload = multer({ dest: "uploads/" });
+
+//500 에러 핸들링
+app.use((err: Error, _req: unknown, res: any, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+  next(err);
+});
+
+// routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.post("/convert", upload.any(), (req, res) => {
+  console.log(req.files, "req.files");
+  console.log(req.body, "req.body");
+
+  res.json(req.files);
 });
 
 app.listen(3000, () => {
